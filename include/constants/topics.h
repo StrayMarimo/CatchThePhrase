382fecd9242bf6e1e45ab223901a7088375baa46
unknown <ilastimosa@gbox.adnu.edu.ph>
2023-11-19 19:00:08 +0800

    Added Topics

--- include/constants/topics.h
+++ include/constants/topics.h
@@ -5,14 +5,70 @@
 
 // TODO: Add topics here
 char topics[MAX_TOPIC][MAX_STRING_SIZE] = {
-    "Movies", 
-    "Song", 
-    "Anime",
-    "TV Series",
-    "Books",
-    "Games",
+    "Movie Title - Action", 
+    "Movie Title - Horror", 
+    "Movie Title - Comedy", 
+    "Movie Title - Romance", 
+    "Movie Title - Drama", 
+    "Movie Title - Fantasy", 
+    "Movie Title - Action/Adventure", 
+    "Movie Title - Musical", 
+    "Movie Title - Fiction",
+    "Movie Title - Science Fiction",
+    "Movie Title - Documentary",
+    "Disney Animated Classics",
+
+    "Song Title - Electronic Dance Music",
+    "Song Title - Rock",
+    "Song Title - Rhythm and Blues",
+    "Song Title - Country",
+    "Song Title - Pop",
+    "Song Title - Reggae",
+    "Song Title - Taylor Swift",
+
+    "Musical Instruments",
+
+    "Bestselling Novels"
+    "Mystery and Thriller Novels"
+    "Science Fiction Books"
+    "Classic Literature Books"
+
+    "American TV Series",
+    "American Sitcoms",
+    "Korean Dramas",
+
+    "Anime Title",
+    "Manga Title",
+
+    "Board Games",
+    "Video Games",
+
+    "Food",
+    "Fruits",
+    "Vegetables",
+    "Desserts and Sweets",
+    "Healthy Eats",
+    "Comfort Foods",
+
+    "Phrases",
+    "Colors",
+
+    "Daily Routines",
+
+    "Famous Places and Landmarks",
+    "Presidents of the Philippines",
+    "Provinces of the Philippines",
+    "Cities of the Philippines",
+    "Provinces of the Philippines",
+    "Regions of the Philippines",
+    "Colleges and Universities in the Philippines",
+    "Capital Cities of Countries",
+
     "Sports",
-    "Food", 
+    "Sports Teams",
+
+    "Body Organs",
+    "Systems of the Human Body",
 };
 
 #endif
