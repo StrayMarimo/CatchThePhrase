8cc2192305b9dfaf9b24f0a7991dd7d6a55c83d8
StrayMarimo <anbasbas@gbox.adnu.edu.ph>
2023-11-21 11:11:34 +0800

    fix: minor allocation bug

--- include/constants/topics.h
+++ include/constants/topics.h
@@ -5,14 +5,145 @@
 
 // TODO: Add topics here
 char topics[MAX_TOPIC][MAX_STRING_SIZE] = {
-    "Movies", 
-    "Song", 
-    "Anime",
-    "TV Series",
-    "Books",
-    "Games",
-    "Sports",
-    "Food", 
+    "Movie Title - Action", 
+    "Movie Title - Horror", 
+    "Movie Title - Comedy", 
+    "Movie Title - Romance", 
+    "Movie Title - Drama", 
+    "Movie Title - Fantasy", 
+    "Movie Title - Action/Adventure", 
+    "Movie Title - Musical", 
+    "Movie Title - Fiction",
+    "Movie Title - Science Fiction",
+    "Movie Title - Documentary",
+    "Disney Animated Classics",
+
+    "Song Title - Electronic Dance Music",
+    "Song Title - Rock",
+    "Song Title - Rhythm and Blues",
+    "Song Title - Country",
+    "Song Title - Pop",
+    "Song Title - Reggae",
+    "Song Title - Taylor Swift",
+    "Music Terminologies",
+    "Music Albums",
+    "Famous Musicians",
+    "Famous Rockbands",
+    "K-Pop Idols",
+
+    "Musical Instruments",
+
+    "Bestselling Novels",
+    "Mystery and Thriller Novels",
+    "Science Fiction Books",
+    "Classic Literature Books",
+    "Famous Authors",
+
+    "American TV Series",
+    "American Sitcoms",
+    "Korean Dramas",
+
+    "Anime Title",
+    "Manga Title",
+
+    "Board Games",
+    "Video Games",
+
+    "Food",
+    "Fruits",
+    "Vegetables",
+    "Desserts and Sweets",
+    "Healthy Eats",
+    "Comfort Foods",
+
+    "Phrases",
+    "Colors",
+
+    "Daily Routines",
+
+    "Famous Places and Landmarks",
+    "Presidents of the Philippines",
+    "Provinces of the Philippines",
+    "Cities of the Philippines",
+    "Provinces of the Philippines",
+    "Regions of the Philippines",
+    "Colleges and Universities in the Philippines",
+    "Capital Cities of Countries",
+
+    "Popular Sports",
+    "Olympic Games",
+    "Sports Teams",
+    "Famous Athletes",
+    "Sport Legends",
+    "Sport Events",
+    "Sports Venues",
+
+    "Body Organs",
+    "Systems of the Human Body",
+    "Senses",
+    "Sensory Organs",
+
+    "Ancient Civilizations",
+    "Notable Historical Figures",
+
+    "Political Ideologies",
+
+    "Popular Esports Games",
+    "Esports Organizations",
+    "Professional Players",
+
+    "Classical, Modern, & Contemporary Artists",
+    "Famous Youtubers",
+    "Famous Vloggers",
+    "K-drama Artists",
+    "Hollywood Actors",
+    "Hollywood Actress",
+
+    "Art Movements and Styles",
+
+    "Grand Prix Circuits",
+    
+    "Famous Brands",
+    "Famous Filipino Brands",
+    "Wealthiest Individuals",
+    "Luxury Brands",
+
+    "World Religions",
+
+    "Marvel Superheroes",
+    "DC Superheroes",
+
+    "Jobs/Occupations",
+    "Hobbies",
+    "Holidays",
+
+    "Space",
+    "Planets",
+    "Constellations",
+
+    "Elements of the Periodic Table ",
+    "Chemical Compounds",
+
+    "Mountains",
+    "Rivers",
+    "Lakes",
+    "Oceans",
+    "Countries",
+
+    "Mythical Creatures",
+    "Fairy Tales",
+
+    "Car Brands",
+    "Smartphone Brands",
+    "Tech Companies",
+
+    "Greek Mythology",
+    "Roman Emperors",
+
+    "Popular Filipino Dishes",
+    "Famous Philippine Landmarks",
+    "Philippine Mythology",
+    "Notable Filipino Heroes",
 };
 
 #endif
